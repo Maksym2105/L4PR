3d2d9bd133f93e206b962e28cb6fbb54fd263972
Maksym2105 <maxdetrai02@gmail.com>
2024-12-09 18:22:07 +0200

    added findSymbolOccurance

--- src/app/L4PR.java
+++ src/app/L4PR.java
@@ -3,5 +3,16 @@ package app;
 public class L4PR {
     public static void main(String[] args) {
 
+        System.out.println(findSymbolOccurance("Hello World" ,  'o'));
+
+    }
+    public static int findSymbolOccurance(String str , char ch){
+        int count = 0;
+        for(char c : str.toCharArray()){
+            if(ch == c){
+                count++;
+            }
+        }
+        return count;
     }
 }
